e04cef14aa679210a578f04012c9e844cf2727f2
MrGarrison7212 <bokipfc1992@gmail.com>
2026-01-12 06:00:47 +0100

    update model/entity classes.

--- src/main/java/com/example/datacenter_backend_api/persistence/model/DevicePlacementEntity.java
+++ src/main/java/com/example/datacenter_backend_api/persistence/model/DevicePlacementEntity.java
@@ -1,4 +1,30 @@
 package com.example.datacenter_backend_api.persistence.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "device_placement", indexes = {
+        @Index(name = "placement_by_device_idx", columnList = "device_id", unique = true),
+        @Index(name = "placement_by_rack_idx", columnList = "rack_id")
+})
 public class DevicePlacementEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "rack_id", nullable = false)
+    private RackEntity rack;
+
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "device_id", nullable = false)
+    private DeviceEntity device;
+
+    @Column(name = "start_unit", nullable = false)
+    private int startUnit;
 }
--- src/main/java/com/example/datacenter_backend_api/persistence/model/RackEntity.java
+++ src/main/java/com/example/datacenter_backend_api/persistence/model/RackEntity.java
@@ -1,4 +1,34 @@
 package com.example.datacenter_backend_api.persistence.model;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "rack", indexes = {
+        @Index(name = "rack_by_serial_idx", columnList = "serial_number", unique = true)
+})
 public class RackEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 120)
+    private String name;
+
+    @Column(name = "description", length = 2000)
+    private String description;
+
+    @Column(name = "serial_number", nullable = false, length = 120)
+    private String serialNumber;
+
+    @Column(name = "total_units", nullable = false)
+    private int totalUnits;
+
+    @Column(name = "max_power_w", nullable = false)
+    private int maxPowerW;
 }
