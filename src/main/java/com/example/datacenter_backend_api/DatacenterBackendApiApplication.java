bd01abc6734e7aa299ea91ba45576e8e7a9a6e76
MrGarrison7212 <bokipfc1992@gmail.com>
2026-01-12 03:29:04 +0100

    Initial commit.

---
+++ src/main/java/com/example/datacenter_backend_api/DatacenterBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacenter_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatacenterBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatacenterBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datacenter_backend_api/DatacenterBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacenter_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatacenterBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
